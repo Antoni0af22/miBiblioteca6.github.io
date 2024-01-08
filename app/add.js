550fe49242712de7f5a8d635ca9938ef368fe412
Antoni0af22 <62048162+Antoni0af22@users.noreply.github.com>
2024-01-08 00:57:16 -0600

    Add files via upload

---
+++ app/add.js
@@ -0,0 +1,28 @@
+import { db } from 'firebase.js'
+import { collection, addDoc } from "firebase/firestore";
+  
+  /**
+   * Save a New Task in Firestore
+   * @param {string} titulo 
+   * @param {string} autor 
+   * @param {string} editorial
+   * @param {string} isbn
+   * @param {Date} fecha
+   */
+  export const saveLibro = (titulo, autor, editorial, isbn, fecha) =>
+    addDoc(collection(db, "libros"), { titulo, autor, editorial, isbn, fecha });
+  
+  const librosForm = document.getElementById("libros-form");
+  
+  librosForm.addEventListener('submit',(e)=>{
+    e.preventDefault()
+
+    const titulo     = librosForm['titulo']
+    const autor      = librosForm['autor']
+    const editorial  = librosForm['editorial']
+    const isbn       = librosForm['isbn']
+    const fecha      = librosForm['fecha']
+
+    saveLibro(titulo.value, autor.value, editorial.value, isbn.value, fecha.value)
+  })
+
