0f2d8eb6a2ed203dc5f3cbd20832b0d71e5f8340
Antoni0af22 <62048162+Antoni0af22@users.noreply.github.com>
2024-01-08 00:58:18 -0600

    Update firebase.js

--- app/firebase.js
+++ app/firebase.js
@@ -2,7 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  // ...
+  apiKey: "AIzaSyAmWyD3GoMybfrr93Fu35bmKX9xgNikjoQ",
+  authDomain: "mibliblioteca6.firebaseapp.com",
+  projectId: "mibliblioteca6",
+  storageBucket: "mibliblioteca6.appspot.com",
+  messagingSenderId: "561002201224",
+  appId: "1:561002201224:web:590174f6a6fbae85b4df3d"
 };
 
 // Initialize Firebase
